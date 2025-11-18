02da49436f3bf0b7892885f606740096969c7312
Rolex-444 <sivaraman.gc49@gmail.com>
2025-11-18 19:29:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -78,6 +78,6 @@ async def on_shutdown():
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.getenv("PORT", "8080"))
+    port = int(os.getenv("PORT", "8000"))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
     
