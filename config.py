6a1106d6aa28d022fc0963ac2c238beae52cfda0
Rolex-444 <sivaraman.gc49@gmail.com>
2025-11-22 14:03:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 API_ID = int(os.getenv("API_ID", ""))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+CHANNEL_ID = os.getenv("CHANNEL_ID", "")  # <-- Add this line!
 
 VERIFICATION_DEFAULT_ENABLED = True
 VERIFICATION_DEFAULT_FREE_LIMIT = 3
@@ -14,7 +15,8 @@ SHORTLINK_API = os.getenv("SHORTLINK_API", "")
 SHORTLINK_URL = os.getenv("SHORTLINK_URL", "")
 
 # Add this line
-BOT_USERNAME = os.getenv("BOT_USERNAME", "Movie_magic_club_bot")  # Without @
+BOT_USERNAME = os.getenv("BOT_USERNAME", "Movie_magic_club_bot") # Without @
 
 if not API_ID or not API_HASH or not BOT_TOKEN:
     print("❌ Please set API_ID, API_HASH, BOT_TOKEN as env variables")
+    
